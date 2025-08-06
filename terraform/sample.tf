74ae89b4e6d2e4a7ffa8ae3b5edfcb9a7e505e89
suraj <surajbele51@gmail.com>
2025-08-06 23:24:26 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -10,7 +10,6 @@ provider "aws" {
 }
 resource "aws_instance" "myinstance" {
     ami = "ami-020cba7c55df1f615"
-    name = myinstance
     instance_type = "t2.micro"
     key_name = "id_rsa"
     vpc_security_group_ids = ["sg-0d34c3d2b6fa6492a", "sg-0d34c3d2b6fa6492a"]
