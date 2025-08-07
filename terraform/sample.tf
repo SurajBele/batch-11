edfefe7061c9401126e2ea52df09c9086024d4e3
suraj <surajbele51@gmail.com>
2025-08-07 09:03:18 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -20,7 +20,14 @@ resource "aws_instance" "myinstance" {
 }
 
 resource "aws_security_group" "name" {
-  
+ 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 variable "instance_type" {
