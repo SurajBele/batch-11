bab96b20a72c02ff69fce9040112bc83417044a3
suraj <surajbele51@gmail.com>
2025-08-07 09:12:42 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "terra-sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
