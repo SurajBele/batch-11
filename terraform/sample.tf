d239cf007c2c62c11d06727ac0163bafe26214eb
suraj <surajbele51@gmail.com>
2025-08-07 09:09:25 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myinstance" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_pair
-    vpc_security_group_ids = aws_security_group.terra-sg
+    vpc_security_group_ids = aws_security_group.terra-sg.id
     tags = {
       Name = "spider-terraform"
       env = "devops"
