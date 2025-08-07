fb1829490e9105a8c8791daf4cbc0db2b22be3a7
suraj <surajbele51@gmail.com>
2025-08-07 08:45:53 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -9,7 +9,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myinstance" {
-    ami = "ami-020cba7c55df1f615"
+    ami = 
     instance_type = var.instance_type
     key_name = "id_rsa"
     vpc_security_group_ids = ["sg-0d34c3d2b6fa6492a", "sg-0d34c3d2b6fa6492a"]
@@ -23,7 +23,7 @@ variable "instance_type" {
   default = "t2.micro"
 }
 variable "ami" {
-  default = 
+  default = "ami-020cba7c55df1f615"
 }
 output "instance_public_ip" {
   default = 
