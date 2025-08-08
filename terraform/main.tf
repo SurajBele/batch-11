db5bac89c0184b82b3bb04542acc6094b1786162
suraj <surajbele51@gmail.com>
2025-08-08 08:48:51 +0530

    --- Auto Git Commit ---

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myinstance" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_pair
-    vpc_security_group_ids = [aws_security_group.terra-sg.id]
-    tags = {
-      Name = "spider-terraform"
-      env = "devops"
-    } 
+resource "" "name" {
+  
 }
