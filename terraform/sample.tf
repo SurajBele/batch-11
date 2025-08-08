3c836305f3947385de64074c03faa186c6b70158
suraj <surajbele51@gmail.com>
2025-08-08 08:27:35 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -8,10 +8,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-provider "google" {
-  project     = "my-project-id"
-  region      = "us-central1"
-}
+
 resource "aws_instance" "myinstance" {
     ami = var.ami
     instance_type = var.instance_type
@@ -22,52 +19,7 @@ resource "aws_instance" "myinstance" {
       env = "devops"
     } 
 }
-resource "google_service_account" "default" {
-  account_id   = "my-custom-sa"
-  display_name = "Custom SA for VM Instance"
-}
 
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
 resource "aws_security_group" "terra-sg" {
   ingress {
     from_port       = 80
