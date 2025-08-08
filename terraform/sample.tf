b31e519f0a6dee374fa20e2c97e65ece31a256db
suraj <surajbele51@gmail.com>
2025-08-08 08:45:14 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,3 +1,13 @@
+terraform {
+    backend "s3" {
+        bucket = "batch-11-terraform"
+        region = "us-east-1"
+        key = "terraform.tfstate" 
+    }
+}
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_instance" "myinstance" {
     ami = var.ami
