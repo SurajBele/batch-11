5bd70f79f0673e7c80c287e3b96a621f12db1ffa
suraj <surajbele51@gmail.com>
2025-08-11 08:32:32 +0530

    --- Auto Git Commit ---

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,8 @@ resource "aws_internet_gateway" "myigw" {
   }
 }
 resource "aws_route_table" "myroute" {
-  vpc_id = aws_vpc.myvpc.id
-  route {
+            vpc_id = aws_vpc.myvpc.id
+              route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myigw.id
   }
