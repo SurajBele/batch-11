15ad5866f515c9b2b5ab561bfa6eedd10e3d525b
suraj <surajbele51@gmail.com>
2025-08-11 08:47:05 +0530

    --- Auto Git Commit ---

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -15,17 +15,3 @@ variable "key_pair"{
 variable "aws_vpc_cidr" {
   default = "10.0.0.0/16"
 }
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "ami" {
-  default = "ami-020cba7c55df1f615"
-}
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "key_pair"{
-    default = "test"
-}
