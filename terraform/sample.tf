ee4776caf01508f037660228666e08f27ee17899
suraj <surajbele51@gmail.com>
2025-08-11 08:45:45 +0530

    --- Auto Git Commit ---

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -56,21 +56,3 @@ resource "aws_security_group" "terra-sg" {
     Name = "terra-sg"
   }
 }
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "ami" {
-  default = "ami-020cba7c55df1f615"
-}
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "key_pair"{
-    default = "test"
-}
-
-output "instance_public_ip" {
-  value = aws_instance.myinstance.public_ip
-}
