329c122021cdb9e84f71da7bf9e8dcd9cc384277
suraj <surajbele51@gmail.com>
2025-08-12 08:52:30 +0530

    --- Auto Git Commit ---

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-# terraform {
-#     backend "s3" {
-#         bucket = "batch-11-terraform"
-#         region = "us-east-1"
-#         key = "terraform.tfstate" 
-#     }
-# }
-# provider "aws" {
-#   region = "us-east-1"
-# }
+terraform {
+    backend "s3" {
+        bucket = "batch-11-terraform"
+        region = "us-east-1"
+        key = "terraform.tfstate" 
+    }
+}
+provider "aws" {
+  region = "us-east-1"
+}
