f17647c886f0b3e1d428dfdd5395db300b293bd0
suraj <surajbele51@gmail.com>
2025-08-12 09:02:50 +0530

    --- Auto Git Commit ---

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,17 +1,30 @@
-variable "instance_type" {
-  default = "t2.micro"
+variable "region" {
+  default = "ap-south-1"
+}
+variable "project" {
+  default = "cloudblitz"
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "private_cidr" {
+  default = "10.0.0.0/24"
+}
+variable "public_cidr" {
+  default = "10.0.1.0/24"
+}
+variable "environment" {
+  default = "devops"
+}
+variable "instance_count" {
+  default = "2"
 }
 variable "image_id" {
-  default = "ami-020cba7c55df1f615"
+  default = "ami-0f5ee92e2d63afc18"
 }
-variable "region" {
-  default = "us-east-1"
+variable "key_pair" {
+  default = "id_rsa"
 }
-
-variable "key_pair"{
-    default = "id_rsa"
-}
-
-variable "aws_vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "instance_type" {
+  default = "t2.micro"
 }
