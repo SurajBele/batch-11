1cf1cb0c470900b97a756467aad540b6820576ca
suraj <surajbele51@gmail.com>
2025-08-21 09:10:19 +0530

    --- Auto Git Commit ---

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull') { 
             steps {
-                
+                git clone url
                 echo "pulling successful" 
             }
         }
