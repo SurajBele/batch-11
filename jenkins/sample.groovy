09112bc09d879201230f30478737ee33a8753eca
suraj <surajbele51@gmail.com>
2025-08-28 08:40:44 +0530

    --- Auto Git Commit ---

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -25,7 +25,7 @@ pipeline {
         }
         stage('quality-gate') { 
             steps {
-                waitForQualityGate abortPipeline: false, credentialsId: 'sonar-token'
+                waitForQualityGate abortPipeline: tru, credentialsId: 'sonar-token'
                 echo "quality-gate successful" 
             }
         }
